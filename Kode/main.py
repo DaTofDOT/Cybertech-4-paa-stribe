de2f2808618b2b9cdf1d5f9db35116dc410cbb6c
Tommy Krogh Andersen <tommy_k_andersen@hotmail.com>
2025-01-09 12:38:23 +0100

    updated player UI

--- Kode/main.py
+++ Kode/main.py
@@ -6,7 +6,6 @@ from PyQt6.QtWidgets import *
 if __name__ == "__main__":
     app = QApplication([])
     gui = player.player()
-    gui.setBaseSize(QSize(800, 600))
     app.aboutToQuit.connect(gui.closeEvent)
     gui.show() 
     app.exec()
--- Kode/player.py
+++ Kode/player.py
@@ -2,6 +2,7 @@ import game
 
 from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
 
 
 class player(QMainWindow):
@@ -10,7 +11,40 @@ class player(QMainWindow):
     '''
     def __init__(self):
         super().__init__()
-        pass
+        central = QWidget(self)
+        self.setCentralWidget(central)
+
+        # font
+        info_font = QFont("Lucida Sans Typewriter", 18, QFont.Weight.Bold)
+        # UI
+        self.setMinimumSize(600, 400) # min size
+        self.setWindowTitle("4-paa-stribe") # window title
+        self.info_label = QLabel("4-paa-stribe") # info label
+        self.info_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.info_label.setFont(info_font)
+        self.info_label.setMaximumHeight(100)
+
+        
+
+
+
+        
+
+        # Game board
+        self.btns = [QPushButton() for _ in range(42)]
+        grid = QGridLayout()
+        for i, btn in enumerate(self.btns):
+            grid.addWidget(btn, i // 7, i % 7)
+            btn.clicked.connect(lambda _, idx=i+1: self.handle_button_click(idx))
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.info_label)
+        main_layout.addLayout(grid)
+        central.setLayout(main_layout)
+
+
+        def handle_button_click(idx):
+            pass
     
     def closeEvent(self, a0=0):
         
